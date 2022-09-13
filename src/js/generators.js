2e97685e7fbe0ac8b11f71884d4b5f6e10a65aef
Alexandra Chernova <estelelendil@mail.ru>
2022-09-13 14:41:25 +0300

    task two-added functions generators

--- src/js/generators.js
+++ src/js/generators.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-plusplus */
+/* eslint-disable max-len */
 /**
  * Формирует экземпляр персонажа из массива allowedTypes со
  * случайным уровнем от 1 до maxLevel
@@ -9,7 +11,17 @@
  *
  */
 export function* characterGenerator(allowedTypes, maxLevel) {
+  while (true) {
+    const randomFerst = Math.round(Math.random(allowedTypes.length));
+    let randomSecond = Math.round(Math.random(maxLevel));// как сделать чтоб не округлял до нуля
+    if (randomSecond === 0) {
+      randomSecond = Math.round(Math.random(maxLevel));
+    }
+    const Char = allowedTypes[randomFerst];
+    yield new Char(randomSecond);
+  }
   // TODO: write logic here
+  // должен быть дурацкий счетчик с next(
 }
 
 /**
@@ -20,5 +32,12 @@ export function* characterGenerator(allowedTypes, maxLevel) {
  * @returns экземпляр Team, хранящий экземпляры персонажей. Количество персонажей в команде - characterCount
  * */
 export function generateTeam(allowedTypes, maxLevel, characterCount) {
+  // debugger;
+  const playerGenerator = characterGenerator(allowedTypes, maxLevel);
+  const team = [];
+  for (let i = 0; i < characterCount; i++) {
+    team.push(playerGenerator.next().value);// почему-то возвращается только первый объект
+  }
+  return team;
   // TODO: write logic here
 }
