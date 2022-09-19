ea4a65b046d444a48946906a6452aa7222f65fe9
Alexandra Chernova <estelelendil@mail.ru>
2022-09-19 18:55:18 +0300

    initialize GameState

--- src/js/GameState.js
+++ src/js/GameState.js
@@ -1,7 +1,19 @@
 export default class GameState {
+  constructor(isPlayerActive, choosenHero) {
+    this.isPlayerActive = isPlayerActive;
+    this.choosenHero = choosenHero;
+  }
+
   // eslint-disable-next-line no-unused-vars
   static from(object) {
-    // TODO: create object
-    return null;
+    if (object == null || typeof object !== 'object') {
+      return new GameState(true, null);
+    }
+    if (object.choosenHero && object.isPlayerActive) {
+      return new GameState(object.isPlayerActive, object.choosenHero);
+    }
+    return new GameState(true, null);
   }
 }
+// хранить , чей ход в игре
+// хранить выделенного игрока
