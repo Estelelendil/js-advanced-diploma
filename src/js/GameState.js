344521e396045060d7ca7e1208f027e4373e6232
Alexandra Chernova <estelelendil@mail.ru>
2022-09-23 09:55:54 +0300

    added props choosenHero and position

--- src/js/GameState.js
+++ src/js/GameState.js
@@ -1,19 +1,21 @@
 export default class GameState {
-  constructor(isPlayerActive, choosenHero) {
+  constructor(isPlayerActive, choosenHero, position) {
     this.isPlayerActive = isPlayerActive;
+    this.position = position;
     this.choosenHero = choosenHero;
   }
 
   // eslint-disable-next-line no-unused-vars
   static from(object) {
     if (object == null || typeof object !== 'object') {
-      return new GameState(true, null);
+      return new GameState(true, null, null);
     }
-    if (object.choosenHero && object.isPlayerActive) {
-      return new GameState(object.isPlayerActive, object.choosenHero);
+    if (object.choosenHero && object.isPlayerActive && object.position) {
+      return new GameState(object.isPlayerActive, object.choosenHero, object.position);
     }
-    return new GameState(true, null);
+    return new GameState(true, null, null);
   }
 }
 // хранить , чей ход в игре
-// хранить выделенного игрока
+// хранить выделенного персонажа
+// хранить позицию выбранного персонажа
