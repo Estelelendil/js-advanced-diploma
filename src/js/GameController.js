3c58afe964418222de30da90e21d36ef4c1c4b53
Alexandra Chernova <estelelendil@mail.ru>
2022-09-23 09:56:47 +0300

    restructured code of method `onClellEnter`

--- src/js/GameController.js
+++ src/js/GameController.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 /* eslint-disable max-len */
 /* eslint-disable no-shadow */
 /* eslint-disable no-unused-vars */
@@ -8,12 +9,16 @@ import Magician from './characters/magician';
 import Swordsman from './characters/swordman';
 import Undead from './characters/undead';
 import Vampire from './characters/vampire';
+import cursors from './cursors';
 import GameState from './GameState';
 import { generateTeam } from './generators';
 import PositionedCharacter from './PositionedCharacter';
 import Team from './Team';
 import themes from './themes';
-import { randomizer, randomPosition, uniqueArr } from './utils';
+import {
+  chooseYourSide,
+  generateLegalCells, randomizer, randomPosition, uniqueArr,
+} from './utils';
 
 const goodPlayerTypes = [Magician, Swordsman, Bowman]; // доступные классы игрока
 const badPlayerTypes = [Daemon, Undead, Vampire];
@@ -42,6 +47,7 @@ export default class GameController {
     this.gamePlay = gamePlay;
     this.stateService = stateService;
     this.state = null;
+    this.arrPositions = arrPositions;
   }
 
   init() {
@@ -55,7 +61,10 @@ export default class GameController {
 
     const stage = this.stateService.load();
     console.log(stage);
-    this.state = GameState.from(stage);
+    this.stateService.clean();
+    this.state = new GameState(true, null, null);
+    // this.state = GameState.from(stage);
+    this.state.position = null;
     // загружать новое состояние из this. stateService
     // с помощью объекта класса GameState нужно вытаскивать конкретные данные
   }
@@ -66,13 +75,12 @@ export default class GameController {
 
     arrPositions.find((item) => {
       if (item.position === index && (item.character.type === 'bowman' || item.character.type === 'swordsman' || item.character.type === 'magician')) {
-        if (this.state.choosenHero) {
-          this.gamePlay.deselectCell(this.state.choosenHero);// снимаем выделеннного ранее персонажа,если таковой есть
+        if (this.state.position) {
+          this.gamePlay.deselectCell(this.state.position);// снимаем выделеннного ранее персонажа,если таковой есть
         }
-        console.log(index);
         this.gamePlay.selectCell(index);// устанавливаем нового
-        // eslint-disable-next-line no-debugger
-        this.state.choosenHero = index;// записываем в state
+        this.state.position = index;// записываем в state
+        this.state.choosenHero = item.character;
         this.stateService.save(this.state); // сохраняем
         flag = true;
         return flag;
@@ -83,19 +91,53 @@ export default class GameController {
       return this.gamePlay.constructor.showError('в данной клетке нет доступного персонажа');
     }
     return null;
-    // TODO: react to click
   }
 
   onCellEnter(index) {
-    // console.log(index);
-    let message;
-    arrPositions.map((item) => {
-      if (item.position === index) {
-        message = `\u{1F396}${item.character.level} \u{2694}${item.character.attack} \u{1F6E1}${item.character.defence} \u{2764}${item.character.health}`;
-        this.gamePlay.showCellTooltip(message, index);// почему this здесь не определяется?
-      } return index;
-    });
-    // TODO: react to mouse enter
+    const legalCellMovies = generateLegalCells(this.state.position, this.state.choosenHero.distanseMovie);
+    const legalCellAttack = generateLegalCells(this.state.position, this.state.choosenHero.distanseAttack);
+
+    this.gamePlay.setCursor(cursors.notallowed);
+
+    const maybeCharacter = this.arrPositions.find((item) => item.position === index)?.character;// проверяем наводим ли мы на перса, сохраняем перса, на которого навели или ничего
+
+    if (this.state.choosenHero) { // если есть выделенный персонаж
+      if (!maybeCharacter) { // если нет, то проверяем возможность перехода
+        if (legalCellMovies.includes(index)) {
+          this.gamePlay.setCursor(cursors.pointer);
+        }
+      } else if (chooseYourSide(maybeCharacter)) { // смотрим хороший или плохой
+        this.gamePlay.setCursor(cursors.pointer);// если хороший то всегда можем выбрать
+      } else if (legalCellAttack.includes(index)) { // если плохой, то можем ли атаковать
+        this.gamePlay.setCursor(cursors.crosshair);
+      }
+    }
+    // если выделенного персонажа в сторе нет
+    const message = `\u{1F396}${maybeCharacter.level} \u{2694}${maybeCharacter.attack} \u{1F6E1}${maybeCharacter.defence} \u{2764}${maybeCharacter.health}`;
+    this.gamePlay.showCellTooltip(message, index);
+    // TODO: почему не высвечивается курсор на атаку
+    // как чистить стор
+
+    // arrPositions.find((item) => {
+    //   if (item.position === index && (item.character.type === 'bowman' || item.character.type === 'swordsman' || item.character.type === 'magician')) {
+    //     this.gamePlay.setCursor(cursors.pointer);// проверка на курсор смену персонажа
+    //   }
+    //   if (legalCellMovies.includes(index)) {
+    //     this.gamePlay.setCursor(cursors.pointer);
+    //   }
+    //   if (legalCellAttack.includes(index)) {
+    //     if (item.position === index && (item.character.type === 'daemon' || item.character.type === 'vampire' || item.character.type === 'undead')) {
+    //       console.log(item.character.type);// на атаку
+    //       this.gamePlay.setCursor(cursors.crosshair);
+    //     }
+    //     console.log(legalCellMovies);
+    //   }
+
+    //   if (legalCellMovies.includes(index)) { // просто на клетку для перемещения и это правило перебивает все остальные
+    //     this.gamePlay.setCursor(cursors.pointer);
+    //   } else { this.gamePlay.setCursor(cursors.notallowed); }
+    //   return null;
+    // });
   }
 
   onCellLeave(index) {
